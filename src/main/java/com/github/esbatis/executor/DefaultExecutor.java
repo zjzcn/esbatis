f012501380b2beae4567f25ba3b9224b2a28c724
jinzhong.zhang <jinzhong.zhang@ele.me>
2017-09-15 11:07:29 +0800

    modify log

--- src/main/java/com/github/esbatis/executor/DefaultExecutor.java
+++ src/main/java/com/github/esbatis/executor/DefaultExecutor.java
@@ -65,7 +65,7 @@ public class DefaultExecutor implements Executor {
         if (handler == null) {
             handler = new DefaultResultHandler(ms);
         } else {
-            logger.debug("ResultHandler[{}] used for method[{}].", handler.getClass(), mapperMethod.getName());
+            logger.debug("ResultHandler[{}] used for statement[{}].", handler.getClass(), ms.getStatement());
         }
 
         return (T) handler.handleResult(resp);
